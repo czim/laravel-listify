aaef5544a53733cdbe56b2343b0960a2cf91711d
Coen Zimmerman <coen@pixelindustries.com>
2016-10-14 23:02:25 +0200

    Added tests for global scoped listify model with hidden records

---
+++ tests/ListifyWithGlobalScopeTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Czim\Listify\Test;
+
+use Czim\Listify\Test\Helpers\TestModel;
+use Czim\Listify\Test\Helpers\TestModelWithGlobalScope;
+
+class ListifyWithGlobalScopeTest extends StandardListifyTest
+{
+
+    /**
+     * @before
+     */
+    protected function seedDatabase()
+    {
+        // set the standard models in the default scope
+        parent::seedDatabase();
+
+        // flag all models as inactive
+        foreach (TestModelWithGlobalScope::all() as $model) {
+            $model->active = false;
+            $model->save();
+        }
+
+        // assert that we did in fact 'hide' them
+        $this->assertEquals(0, TestModelWithGlobalScope::count(), "Setup failed for global scoped models");
+    }
+
+    /**
+     * @param int $id
+     * @return TestModelWithGlobalScope|TestModel
+     */
+    protected function findStandardModel($id)
+    {
+        return TestModelWithGlobalScope::withoutGlobalScopes()->find($id);
+    }
+
+}
